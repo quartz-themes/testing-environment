87a8c7571d2a9e84cd0c6c3cdc8113841921b58d
saberzero1 <github@emilebangma.com>
2025-04-26 22:30:02 +0200

    Update from upstream

--- quartz.config.ts
+++ quartz.config.ts
@@ -86,7 +86,6 @@ const config: QuartzConfig = {
       }),
       Plugin.Assets(),
       Plugin.Static(),
-      Plugin.Favicon(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
       Plugin.CustomOgImages(),
